33365a7806e7a2571adbad4162dbeea0cc660b01
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-04-21 15:04:58 +0530

    e

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Conversation = () => {
   return (
-    <div className='w-full h-auto flex bg-red-800 flex-wrap justify-around'>
-        <div className="left pt-6 pr-52 pb-6">
+    <div className='convo-container w-full h-auto flex flex-col md:flex-row bg-red-800 flex-wrap justify-around'>
+        <div className="convo-content left pt-6 md:pr-52 md:pb-6 pl-16   ">
             <h2 className='italic text-2xl text-white'>Consult and start your fitness journey</h2>
             <h1 className='text-4xl text-white font-bold'>Let's start a Conversation</h1>
         </div>
-        <div className='right pt-10 pb-6'>
+        <div className='convo-btn right pt-5 pb-5 md:pt-10 md:pb-4 pl-16'>
             <button className='bg-transparent px-16 font-semibold hover:bg-white hover:text-black transition-all duration-200 py-2 text-white border-2 border-white rounded-full'>Contact Us</button>
         </div>
     </div>
