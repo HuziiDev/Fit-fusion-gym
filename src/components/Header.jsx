6b4f4d347351207e82c35041632ceb46e9d7ebd3
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-04-21 16:54:05 +0530

    last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,12 @@
-
-'use client';
-import React, { useState } from 'react'
-import Link from 'next/link'
-import Image from "next/image"
-import Navbar from './Navbar';
+import React from 'react'
 
 const Header = () => {
-
-    
-
-
-        
-   
-
   return (
-   
-   
+    <div className='main  h-auto bg-black'>
 
-
-    <div className='hero w-full h-[40rem] bg-black overflow-x-hidden  flex   '> 
-    <div className="hero-content w-[60%] md:w-[50%]   md:block    z-10 content mt-44  ml-44 ">
-        <h1 className='sm:text-4xl md:text-5xl  text-white font-medium'>Fitness and Health</h1>
-        <h2 className='text-white text-3xl mt-4'>With double Power</h2>
-        <button className='px-8 py-2 bg-transparent border-2 rounded mt-4 font-semibold border-red-600 text-white '>Read more &raquo;</button>
+        <video className='header-video w-full' autoPlay loop muted  src="/workout.mp4"/>
     </div>
-    {/* <div className="image  md:absolute right-0  ">
-        <Image className=' opacity-40 md:opacity-80'  src={'/pose.png'} height={'500'} width={'700'}></Image>
-    </div> */}
-
-
-    </div>
-   
   )
 }
 
-export default Header
+export default Header 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
   return (
     <div className='navbar bg-black w-full h-auto  flex justify-between pt-4 flex-wrap'>
     <Link className='text-white text-3xl font-semibold pl-10' href={'/'}>
-      <Image src={'/logo.png'} height={'100'} width={'100'}></Image>
+      <Image className='w-16 h-16 md:w-20 md:h-20' src={'/logo.png'} height={'100'} width={'100'}></Image>
     </Link>
     
     <nav className='hidden md:flex gap-10 pr-10 pt-7'>
@@ -70,15 +70,15 @@ const Navbar = () => {
      
 
         {isActive ?   
-          <div className='md:hidden pr-4 pt-4 cursor-pointer z-30'  onClick={() => setActive((prev)=> !prev)} >
-          <svg fill="#ffffff" width="45px" height="45px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+          <div className='md:hidden  pr-4 pt-4 cursor-pointer z-30'  onClick={() => setActive((prev)=> !prev)} >
+          <svg fill="#ffffff" width="35px" height="35px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
     <path d="M0 14.545L1.455 16 8 9.455 14.545 16 16 14.545 9.455 8 16 1.455 14.545 0 8 6.545 1.455 0 0 1.455 6.545 8z" fill-rule="evenodd"/>
 </svg>
 
 </div>
         
         :<div className='md:hidden pr-4 pt-4 cursor-pointer'  onClick={() => setActive((prev)=> !prev)} >
-    <svg width="50px" height="50px" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="30px" height="30px" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M2 12.32H22" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
   <path d="M2 18.32H22" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
   <path d="M2 6.32001H22" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
